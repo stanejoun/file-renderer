34a6ee5ea2bbff2b2236db6bc6b58b5d7b5115a0
Olivier PRUDENTE <olivier.prudente@gmail.com>
2021-12-15 18:03:27 +0100

    Update

--- src/RendererSettings.php
+++ src/RendererSettings.php
@@ -26,4 +26,64 @@ class RendererSettings
 		}
 		return $array;
 	}
+
+	public function top(?int $top): RendererSettings
+	{
+		$this->top = $top;
+		return $this;
+	}
+
+	public function left(?int $left): RendererSettings
+	{
+		$this->left = $left;
+		return $this;
+	}
+
+	public function width(?int $width): RendererSettings
+	{
+		$this->width = $width;
+		return $this;
+	}
+
+	public function height(?int $height): RendererSettings
+	{
+		$this->height = $height;
+		return $this;
+	}
+
+	public function pdfWidth(?string $pdfWidth): RendererSettings
+	{
+		$this->pdfWidth = $pdfWidth;
+		return $this;
+	}
+
+	public function pdfHeight(?string $pdfHeight): RendererSettings
+	{
+		$this->pdfHeight = $pdfHeight;
+		return $this;
+	}
+
+	public function pageFormat(?int $pageFormat): RendererSettings
+	{
+		$this->pageFormat = $pageFormat;
+		return $this;
+	}
+
+	public function orientation(?string $orientation): RendererSettings
+	{
+		$this->orientation = $orientation;
+		return $this;
+	}
+
+	public function zoomFactor(?int $zoomFactor): RendererSettings
+	{
+		$this->zoomFactor = $zoomFactor;
+		return $this;
+	}
+
+	public function isContent(?int $isContent): RendererSettings
+	{
+		$this->isContent = $isContent;
+		return $this;
+	}
 }
